e1f58b6c99fce260ddca19d437582062d27a8abf
DESKTOP-D467OFD\turpija <turpija@gmail.com>
2023-03-07 13:22:42 +0100

    FK naming

--- SaloonDatabaseQuerry.sql
+++ SaloonDatabaseQuerry.sql
@@ -11,15 +11,22 @@ CREATE TABLE Saloon (
 );
 
 CREATE TABLE Customer (
-	Id UNIQUEIDENTIFIER PRIMARY KEY not null,
-	UserId UNIQUEIDENTIFIER not null,
+	UserId UNIQUEIDENTIFIER PRIMARY KEY not null,
 	SaloonId UNIQUEIDENTIFIER not null,
-	FOREIGN KEY (UserId) REFERENCES UserWeb(Id),
-	FOREIGN KEY (SaloonId) REFERENCES Saloon(Id)
+	CONSTRAINT "FK_Customer_UserWeb_UserId" FOREIGN KEY (UserId) REFERENCES UserWeb(Id),
+	CONSTRAINT "FK_Customer_Saloon_SaloonId" FOREIGN KEY (SaloonId) REFERENCES Saloon(Id)
+);
+
+CREATE TABLE Service (
+	Id UNIQUEIDENTIFIER PRIMARY KEY not null,
+	Name VARCHAR(50) not null,
+	Price DECIMAL not null
 );
 
 CREATE TABLE SaloonService (
 	Id UNIQUEIDENTIFIER PRIMARY KEY not null,
-	Name VARCHAR(50) not null,
-	Price DECIMAL not null
+	SaloonId UNIQUEIDENTIFIER not null,
+	ServiceId UNIQUEIDENTIFIER not null,
+	CONSTRAINT "FK_SaloonService_Saloon_SaloonId" FOREIGN KEY (SaloonId) REFERENCES Saloon(Id),
+	CONSTRAINT "FK_SaloonService_Service_ServiceId" FOREIGN KEY (ServiceId) REFERENCES Service(Id)
 );
