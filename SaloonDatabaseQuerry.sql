099d5023f733fc60d397f8a724a77186aaaddd5f
DESKTOP-D467OFD\turpija <turpija@gmail.com>
2023-03-07 12:57:26 +0100

    more table creating

--- SaloonDatabaseQuerry.sql
+++ SaloonDatabaseQuerry.sql
@@ -1,5 +1,25 @@
 CREATE TABLE Users (
-	id INT PRIMARY KEY not null,
-	username VARCHAR(30) not null
+	id UNIQUEIDENTIFIER PRIMARY KEY not null,
+	username VARCHAR(30) not null,
+	email VARCHAR(60) not null
+);
 
-)
+CREATE TABLE Saloon (
+	id UNIQUEIDENTIFIER PRIMARY KEY not null,
+	name VARCHAR(50) not null,
+	location VARCHAR(89) not null
+);
+
+CREATE TABLE Customers (
+	id UNIQUEIDENTIFIER PRIMARY KEY not null,
+	userId UNIQUEIDENTIFIER not null,
+	saloonId UNIQUEIDENTIFIER not null,
+	FOREIGN KEY (userId) REFERENCES Users(id),
+	FOREIGN KEY (saloonId) REFERENCES Saloon(id)
+);
+
+CREATE TABLE SaloonServices (
+	id UNIQUEIDENTIFIER PRIMARY KEY not null,
+	name VARCHAR(50) not null,
+	price DECIMAL not null
+);
