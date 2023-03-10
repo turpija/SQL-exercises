17e6fee49f7b7b129bd3b54b3fd67c381c85d68c
DESKTOP-D467OFD\turpija <turpija@gmail.com>
2023-03-10 13:44:32 +0100

    inserted all data

--- aukcije_DB.sql
+++ aukcije_DB.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE "iBei"
+﻿CREATE DATABASE "iBei"
 
 CREATE TABLE "User" (
 	"Id" UNIQUEIDENTIFIER NOT NULL PRIMARY KEY,
@@ -21,9 +21,12 @@ CREATE TABLE "Bid" (
 	"UserId" UNIQUEIDENTIFIER NOT NULL,
 	"AuctionId" UNIQUEIDENTIFIER NOT NULL,
 	"BidPrice" DECIMAL(18,2) NOT NULL,
+	"BidTime" DATETIME NOT NULL,
 	CONSTRAINT "FK_Bid_User_UserId" FOREIGN KEY ("UserId") REFERENCES "User"("Id"),
 	CONSTRAINT "FK_Bid_Auction_AuctionId" FOREIGN KEY ("AuctionId") REFERENCES "Auction"("Id")
 );
+
+DROP TABLE "Bid";
 
 CREATE TABLE "Item" (
 	"Id" UNIQUEIDENTIFIER NOT NULL PRIMARY KEY,
@@ -32,3 +35,73 @@ CREATE TABLE "Item" (
 	"Description" TEXT NOT NULL,
 	CONSTRAINT "FK_Item_Auction_Id" FOREIGN KEY ("Id") REFERENCES "Auction"("Id")
 );
+
+CREATE TABLE "Section" (
+	"Id" UNIQUEIDENTIFIER NOT NULL PRIMARY KEY,
+	"Name" VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE "Category" (
+	"Id" UNIQUEIDENTIFIER NOT NULL PRIMARY KEY,
+	"SectionId" UNIQUEIDENTIFIER NOT NULL,
+	"Name" VARCHAR(30) NOT NULL
+	CONSTRAINT "FK_Category_Section_SectionId" FOREIGN KEY ("SectionId") REFERENCES "Section"("Id")
+);
+
+INSERT INTO "Section" VALUES
+(NEWID(), 'Računala'),
+(NEWID(), 'Komponente'),
+(NEWID(), 'Mobiteli');
+
+INSERT INTO "Category" ("Id","SectionId","Name") VALUES
+(NEWID(), (SELECT "Id" FROM "Section" WHERE "Name" = 'Komponente'), 'Monitori'),
+(NEWID(), (SELECT "Id" FROM "Section" WHERE "Name" = 'Komponente'), 'Procesori'),
+(NEWID(), (SELECT "Id" FROM "Section" WHERE "Name" = 'Komponente'), 'SSD'),
+(NEWID(), (SELECT "Id" FROM "Section" WHERE "Name" = 'Komponente'), 'Grafičke kartice'),
+(NEWID(), (SELECT "Id" FROM "Section" WHERE "Name" = 'Mobiteli'), 'Stakla'),
+(NEWID(), (SELECT "Id" FROM "Section" WHERE "Name" = 'Mobiteli'), 'Kućišta');
+
+
+declare @brankoId UNIQUEIDENTIFIER SET @brankoId = NEWID();
+declare @stanislavId UNIQUEIDENTIFIER SET @stanislavId = NEWID();
+declare @juraId UNIQUEIDENTIFIER SET @juraId = NEWID();
+declare @maraId UNIQUEIDENTIFIER SET @maraId = NEWID();
+
+INSERT INTO "User" ("Id","Username","Password","Email","Active") VALUES 
+(@brankoId, 'branko','k4u56','branko@branko.com',0),
+(@stanislavId, 'stanislav','s8d7t','stanislav@stanislav.com',1),
+(@juraId, 'jura','jk456g','jura@jura.com',1),
+(@maraId, 'mara','cv87t','mara@mara.com',1);
+
+
+declare @auction1 UNIQUEIDENTIFIER SET @auction1 = NEWID();
+INSERT INTO "Auction" VALUES 
+(@auction1, @stanislavId, 1, '2023-04-10 20:30:00');
+INSERT INTO "Item" VALUES
+(@auction1, (SELECT "Id" FROM "Category" WHERE "Name" = 'SSD'),'Samsung 250 GB','Kao novi samo jednom isproban');
+
+declare @auction2 UNIQUEIDENTIFIER SET @auction2 = NEWID();
+INSERT INTO "Auction" VALUES 
+(@auction2, @juraId, 1, '2023-05-11 20:30:00');
+INSERT INTO "Item" VALUES
+(@auction2, (SELECT "Id" FROM "Category" WHERE "Name" = 'Stakla'),'Staklo za iphone 22','NOVO s računom!');
+
+declare @auction3 UNIQUEIDENTIFIER SET @auction3 = NEWID();
+INSERT INTO "Auction" VALUES 
+(@auction3, @juraId, 1, '2023-06-11 20:00:00');
+INSERT INTO "Item" VALUES
+(@auction3, (SELECT "Id" FROM "Category" WHERE "Name" = 'Kućišta'),'Futrola za iphone SE','Kožna futrola, roza boje s cvjetićima');
+
+INSERT INTO "Bid" VALUES 
+(NEWID(),@maraId, @auction3, 34.55, '2023-03-10 20:30:00'),
+(NEWID(),@juraId, @auction3, 44.55, '2023-03-10 20:31:00'),
+(NEWID(),@maraId, @auction3, 70, '2023-03-10 20:40:00');
+
+INSERT INTO "Bid" VALUES 
+(NEWID(),@stanislavId, @auction1, 20, '2023-03-09 15:30:00'),
+(NEWID(),@juraId, @auction1, 20.01, '2023-03-09 15:40:00');
+
+
+INSERT INTO "Bid" VALUES 
+(NEWID(),@maraId, @auction1, 25, '2023-03-10 08:08:00'),
+(NEWID(),@juraId, @auction1, 25.01, '2023-03-10 09:09:00');
