49735efe90ceb4d9dc67168bb8dd9d14858691f3
DESKTOP-D467OFD\turpija <turpija@gmail.com>
2023-03-13 12:31:34 +0100

    created and connected all tables

--- Trgovina.sql
+++ Trgovina.sql
@@ -23,12 +23,22 @@ ALTER TABLE "Osoba"
 ALTER TABLE "Osoba"
 	ALTER COLUMN "Prezime" NVARCHAR(42);
 
+CREATE TABLE "Artikl" (
+	"Id" INT NOT NULL PRIMARY KEY,
+	"Artikl" VARCHAR(50) NOT NULL,
+	"MjernaJedinca" VARCHAR(10) NOT NULL,
+	"Cijena" FLOAT NOT NULL,
+	"PDV" FLOAT NOT NULL
+);
+
 CREATE TABLE "Narudzba" (
 	"Id" INT NOT NULL PRIMARY KEY,
 	"Broj" VARCHAR(30) NOT NULL,
 	"Datum" DATETIME NOT NULL,
 	"NaruciteljId" INT NOT NULL,
 	"ProdavacId" INT NOT NULL 
+	CONSTRAINT "FK_Narudzba_Osoba_NaruciteljId" FOREIGN KEY ("NaruciteljId") REFERENCES "Osoba"("Id"),
+	CONSTRAINT "FK_Narudzba_Osoba_ProdavacId" FOREIGN KEY ("ProdavacId") REFERENCES "Osoba"("Id")
 );
 
 CREATE TABLE "NarudzbaArtikl" (
@@ -36,4 +46,6 @@ CREATE TABLE "NarudzbaArtikl" (
 	"NarudzbaId" INT NOT NULL,
 	"ArtiklId" INT NOT NULL,
 	"Kolicina" FLOAT NOT NULL
+	CONSTRAINT "FK_NarudzbaArtikl_Narudzba_NarudzbaId" FOREIGN KEY ("NarudzbaId") REFERENCES "Narudzba"("Id"),
+	CONSTRAINT "FK_NarudzbaArtikl_Artikl_ArtiklId" FOREIGN KEY ("ArtiklId") REFERENCES "Artikl"("Id")
 );
